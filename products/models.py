ca18df7df81c974f118e43167e8fb0abce79c8b1
jodi mohammed <jodimohammed82@gmail.com>
2023-12-25 23:48:18 -0500

    creating migrations

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_lenght=255)
+    name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.CharField(max_lenght=2083)
+    image_url = models.CharField(max_length=2083)
