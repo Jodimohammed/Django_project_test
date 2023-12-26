835ee5fdaaf9fe901c4e6ae46d08130f34a60981
jodi mohammed <jodimohammed82@gmail.com>
2023-12-25 23:24:43 -0500

    still working on product

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
+from django.http  import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+#/products --> index
+def index(request):
+    return HttpResponse('hello world')
+
+
+def new(request):
+    return HttpResponse('New Products')
